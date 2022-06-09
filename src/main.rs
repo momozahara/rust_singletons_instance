53f44ead1b21217f39f53890c1e56d877b5ab578
Pream Pinbut <momozahara@hotmail.com>
2022-06-09 18:05:26 +0700

    add readme and rename variable

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@ mod test;
 
 fn main() {
 
-    let mut _game = game::new("Game");
+    let game = game::new("Game");
 
-    println!("{}", _game.get_window_name());
+    println!("{}", game.get_window_name());
 
-    _game.set_window_name("Game 2");
+    game.set_window_name("Game 2");
 
-    println!("{}", _game.get_window_name());
+    println!("{}", game.get_window_name());
 
-    let mut _game_instance = game::get_instance();
+    let game_instance = game::get_instance();
 
-    println!("{}", _game_instance.get_window_name());
+    println!("{}", game_instance.get_window_name());
 
-    _game_instance.set_window_name("Game 3");
+    game_instance.set_window_name("Game 3");
 
-    println!("{}", _game_instance.get_window_name());
+    println!("{}", game_instance.get_window_name());
 
     test::test();
 
--- src/test.rs
+++ src/test.rs
@@ -2,10 +2,10 @@ use crate::game;
 
 pub fn test() {
 
-    let mut _game = game::get_instance();
+    let game = game::get_instance();
 
-    _game.set_window_name("Game 4");
+    game.set_window_name("Game 4");
 
-    println!("{}", _game.get_window_name());
+    println!("{}", game.get_window_name());
 
 }
