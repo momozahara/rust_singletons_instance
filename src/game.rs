ff5efad812504a6e399eb5bfa3c987bd0f319aa9
Pream Pinbut <momozahara@hotmail.com>
2022-06-09 21:36:02 +0700

    P A N I K

--- src/game.rs
+++ src/game.rs
@@ -5,6 +5,16 @@ pub struct Game {
     window_name: &'static str,
 }
 
+impl Game {
+    pub fn get_window_name(&self) -> &'static str {
+        self.window_name
+    }
+
+    pub fn set_window_name(&mut self, window_name: &'static str) {
+        self.window_name = window_name;
+    }
+}
+
 pub fn new(window_name: &'static str) -> &'static mut Game {
     unsafe {
         if GAME_INSTANCE.is_none() {
@@ -16,16 +26,9 @@ pub fn new(window_name: &'static str) -> &'static mut Game {
 
 pub fn get_instance() -> &'static mut Game {
     unsafe {
+        if GAME_INSTANCE.is_none() {
+            panic!("Game instance is not initialized");
+        }
         GAME_INSTANCE.as_mut().unwrap()
-    }
-}
-
-impl Game {
-    pub fn get_window_name(&self) -> &'static str {
-        self.window_name
-    }
-
-    pub fn set_window_name(&mut self, window_name: &'static str) {
-        self.window_name = window_name;
     }
 }
